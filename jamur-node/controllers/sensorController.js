c5694dbbd1bd215d6fb3f2b6802dbb8fd1bb848f
MuhammadNaufalGhulamF20230140073 <naufalfakhrudddin@gmail.com>
2026-01-13 01:41:35 +0700

    fungsi ambil data sensor terbaru

---
+++ jamur-node/controllers/sensorController.js
@@ -0,0 +1,18 @@
+const db = require("../config/db");
+
+// 1. Ambil HANYA 1 data terbaru (Untuk Dashboard Real-time)
+exports.getLatestData = async (req, res) => {
+  try {
+    const [rows] = await db.query(
+      "SELECT temperature, humidity, created_at FROM sensor_data ORDER BY created_at DESC LIMIT 1"
+    );
+
+    if (rows.length > 0) {
+      res.json(rows[0]);
+    } else {
+      res.status(404).json({ message: "Data sensor masih kosong" });
+    }
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
