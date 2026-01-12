510135b6d9f7a51f3a8d4d70cfcafe2a69983fac
Wahyu Agung Prasetyo <wahyu.agung.ft23@mail.umy.ac.id>
2026-01-13 02:27:12 +0700

    menambahkan logika fetchData untuk mengambil data suhu dan kelembaban dari API

--- jamur-react/src/pages/PublicMonitor.jsx
+++ jamur-react/src/pages/PublicMonitor.jsx
@@ -4,3 +4,14 @@ import { Link } from "react-router-dom";
 const PublicMonitor = () => {
   const [data, setData] = useState({ temperature: 0, humidity: 0 });
   const [lastUpdate, setLastUpdate] = useState("-");
+
+  const fetchData = async () => {
+    try {
+      const res = await fetch("http://localhost:3001/api/sensor/latest");
+      const jsonData = await res.json();
+      setData(jsonData);
+      setLastUpdate(new Date().toLocaleTimeString("id-ID"));
+    } catch (err) {
+      console.error("Gagal ambil data");
+    }
+  };
