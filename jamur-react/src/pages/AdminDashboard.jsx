a1b9015cce2d04f4220e953da1565dec145ca8ce
F4KHR111 <fakhriwirdiyan05@gmail.com>
2026-01-13 02:35:54 +0700

    menambahkan file AdminDashboard.jsx dengan import yang diperlukan

---
+++ jamur-react/src/pages/AdminDashboard.jsx
@@ -0,0 +1,4 @@
+import { useState, useEffect, useRef } from "react";
+import { useNavigate } from "react-router-dom";
+import Chart from "chart.js/auto";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
