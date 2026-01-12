b2e03636d7901e8391a1fff05e7dda56934135e3
MuhammadNaufalGhulamF20230140073 <naufalfakhrudddin@gmail.com>
2026-01-13 01:42:34 +0700

    fungsi tampilkan 20 data sensor terbaru

--- jamur-node/controllers/sensorController.js
+++ jamur-node/controllers/sensorController.js
@@ -16,3 +16,15 @@ exports.getLatestData = async (req, res) => {
     res.status(500).json({ error: err.message });
   }
 };
+
+// Ganti fungsi getAllData Anda dengan ini
+exports.getAllData = async (req, res) => {
+  try {
+    const [rows] = await db.query(
+      "SELECT * FROM sensor_data ORDER BY created_at DESC LIMIT 20"
+    );
+    res.json(rows); // Mengirimkan Array secara transparan
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
