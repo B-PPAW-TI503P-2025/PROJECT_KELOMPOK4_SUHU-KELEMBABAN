00b0d78dcd6181567ddc9b7066153bc0b4fb04ef
Wahyu Agung Prasetyo <wahyu.agung.ft23@mail.umy.ac.id>
2026-01-13 02:23:33 +0700

    menambahkan Login component dengan managemen state untuk username, password, dan error handling

---
+++ jamur-react/src/pages/Login.jsx
@@ -0,0 +1,8 @@
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
+const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
