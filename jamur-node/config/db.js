aebaba6b8fab5ddaa50812f89f1d7a4f0077f1c9
MuhammadNaufalGhulamF20230140073 <naufalfakhrudddin@gmail.com>
2026-01-14 20:37:44 +0700

    perbaikan package lock dan tampilan

--- jamur-node/config/db.js
+++ jamur-node/config/db.js
@@ -2,11 +2,11 @@ const mysql = require("mysql2");
 require("dotenv").config();
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || 'localhost',
-    user: process.env.DB_USER || 'root',      // <--- Pastikan ini DB_USER
-    password: process.env.DB_PASSWORD || '',
-    database: process.env.DB_NAME || 'db_kumbung_jamur',
-    port: process.env.DB_PORT || 3306,         // <--- Pastikan port 3306
+  host: process.env.DB_HOST || "localhost",
+  user: process.env.DB_USER || "root", // <--- Pastikan ini DB_USER
+  password: process.env.DB_PASSWORD || "", // <--- Pastikan ini DB_PASSWORD
+  database: process.env.DB_NAME || "db_kumbung_jamur",
+  port: process.env.DB_PORT || 3309, // <--- Pastikan port 3306
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
@@ -15,9 +15,9 @@ const pool = mysql.createPool({
 // Tes koneksi saat pertama kali dijalankan
 pool.getConnection((err, connection) => {
   if (err) {
-    console.error("Koneksi database gagal (Port 3306):", err.message);
+    console.error("Koneksi database gagal (Port 3309):", err.message);
   } else {
-    console.log("Koneksi database MySQL di port 3306 BERHASIL!");
+    console.log("Koneksi database MySQL di port 3309 BERHASIL!");
     connection.release();
   }
 });
